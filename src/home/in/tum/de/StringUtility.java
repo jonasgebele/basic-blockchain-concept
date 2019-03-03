109d71f9d870cc70744aed33af9962a1b2f9ee49
Jonas Gebele <ge69cas@mytum.de>
2019-03-03 15:10:16 +0100

    updated Blockchain-class

--- src/home/in/tum/de/StringUtility.java
+++ src/home/in/tum/de/StringUtility.java
@@ -1,9 +1,8 @@
 package home.in.tum.de;
 
 import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
+import java.security.*;
+import java.util.Base64;
 
 public class StringUtility {
 
@@ -37,5 +36,37 @@ public class StringUtility {
             System.out.println("Generating of hash-value did not work.");
             throw new RuntimeException(e);
         }
+    }
+
+    public static byte [] applyECDSASig(PrivateKey privateKey, String input){
+        Signature dsa;
+        byte [] output = new byte [0];
+        try{
+            dsa = Signature.getInstance("ECDSA", "BC");
+            dsa.initSign(privateKey);
+            byte [] strByte = input.getBytes();
+            dsa.update(strByte);
+            output = dsa.sign();
+        }
+        catch(Exception e) {
+            throw new RuntimeException(e);
+        }
+        return output;
+    }
+
+    public static boolean verifyECDSASig(PublicKey publicKey, String data, byte [] signature){
+        try{
+            Signature ecdsaVerify = Signature.getInstance("ECDSA", "BC");
+            ecdsaVerify.initVerify(publicKey);
+            ecdsaVerify.update(data.getBytes());
+            return ecdsaVerify.verify(signature);
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getStringFromKey(Key key){
+        return Base64.getEncoder().encodeToString(key.getEncoded());
     }
 }
---
+++ src/home/in/tum/de/Transaction.java
@@ -0,0 +1,48 @@
+package home.in.tum.de;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+public class Transaction {
+
+    public String transactionID; // this is also the hash of the transaction
+    private PublicKey sender;
+    private PublicKey reciepient;
+    private float value;
+    public byte [] signature; // this is to prevent anybody else from spending funds in our wallet
+
+    private ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+    public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+
+    private static int sequence = 0; // a rough count of how many transaction have been generated
+
+    public Transaction (PublicKey from, PublicKey to, float value, ArrayList<TransactionInput> inputs){
+        this.sender = from;
+        this.reciepient = to;
+        this.value = value;
+        this.inputs = inputs;
+    }
+
+    private String calculateHash() {
+        sequence++;
+        return StringUtility.applySHA256(
+                StringUtility.getStringFromKey(sender) +
+                        StringUtility.getStringFromKey(reciepient) +
+                        Float.toString(value) +
+                        sequence
+        );
+    }
+
+    // signs all the data we dont wish to tampered with
+    public void generateSignature(PrivateKey privateKey){
+        String data = StringUtility.getStringFromKey(sender) + StringUtility.getStringFromKey(reciepient);
+        signature = StringUtility.applyECDSASig(privateKey, data);
+    }
+
+    // verifies the data we signed hasnt been tampered with
+    public boolean verifySignature(){
+        String data = StringUtility.getStringFromKey(sender) + StringUtility.getStringFromKey(reciepient);
+        return StringUtility.verifyECDSASig(sender, data, signature);
+    }
+}
